5df04c9b4c69f7d2a9b299d5f8dcd9a078668968
fellipecouto96 <fellipecouto96@gmail.com>
2015-11-06 20:26:55 -0200

    Identação do codigo.

--- awesome-dijkstra/src/Principal.java
+++ awesome-dijkstra/src/Principal.java
@@ -7,7 +7,7 @@ import javax.swing.JOptionPane;
 
 public class Principal {
 
-	// Lê o arquivo do tipo .CVS e monta um vetor de String com os valores
+	// Le o arquivo do tipo .CVS e monta um vetor de String com os valores
 	// recebidos do arquivo
 	public static String[] lerArquivoCVS(String arquivo) {
 		BufferedReader buffer = null;
@@ -45,16 +45,16 @@ public class Principal {
 		String nome = null;
 		while (nome == null || nome.equals("")) {
 			nome = JOptionPane
-					.showInputDialog("Coloque o arquivo .CSV com a matriz de adjacência no diretório raiz do projeto. \nPara informa o nome do arquivo, você deve seguir o exemplo abaixo:\nEX: Grafo Trabalho Dijkstra.csv\n\n\nQual o nome do arquivo?");
+					.showInputDialog("Coloque o arquivo .CSV com a matriz de adjacencia no diretorio raiz do projeto. \nPara informa o nome do arquivo, voce deve seguir o exemplo abaixo:\nEX: Grafo Trabalho Dijkstra.csv\n\n\nQual o nome do arquivo?");
 			if (nome == null || nome.equals("")) {
 				JOptionPane.showMessageDialog(null,
-						"Você não respondeu a pergunta.");
+						"Voce nao respondeu a pergunta.");
 			}
 		}
 		try {
 			Grafo g = new Grafo(lerArquivoCVS(nome));
 			JOptionPane.showMessageDialog(null,
-					"Algoritmo de Dijkstra executado com sucesso!");
+					"Para conferir o resultado favor verificar o arquivo .txt no diretorio raiz do projeto.\n\nAlgoritmo de Dijkstra executado com sucesso!");
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, "Arquivo invalido!");
 		}
